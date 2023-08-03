f74d88d22ff83cca73d0ad62bfb426ccbc307ec6
Jimena Belen <jiment4@hotmail.com>
2023-08-03 13:37:02 -0300

    routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,23 +1,18 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-import { Navbar } from "../ui";
-
-import { LoginPage } from "../auth/pages/LoginPage";
-import { DcPage } from "../heroes/pages/DCPage";
-import { MarvelPage } from "../heroes/pages/MarvelPage";
+import { HeroesRoutes } from "../heroes";
+import { LoginPage } from "../auth";
 
 
 export const AppRouter = () => {
   return (
     <>
-        <Navbar />
 
         <Routes>
-            <Route path="marvel" element={<MarvelPage />} />
-            <Route path="dc" element={<DcPage />} />
-
             <Route path="login" element={<LoginPage />} />
-            <Route path="/" element={ <Navigate to="/marvel" />} />
+
+            <Route path="/*" element={<HeroesRoutes />} />
+
         </Routes>
     </>
   )
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,14 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate("/login");
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             
@@ -39,7 +46,8 @@ export const Navbar = () => {
                     </span>
 
                     <button
-                        className='nav-item nav-link btn'>
+                        className='nav-item nav-link btn'
+                        onClick={onLogout}>
                             Logout
                     </button>
 
